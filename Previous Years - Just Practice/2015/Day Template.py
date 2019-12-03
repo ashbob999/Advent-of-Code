ac04bc20aac216a8727c68adf03fe0cf0e968926
ashbob999 <ashbob999@gmail.com>
2019-12-03 15:16:59 +0000

    Added Day Template

---
+++ Previous Years - Just Practice/2015/Day Template.py
@@ -0,0 +1,13 @@
+# link to Day 1: https://adventofcode.com/2015/day/X
+
+# splits the input file into a list of lines
+with open("inputs/Day_X_Input.txt", "r") as file:
+    lines = [line.strip() for line in file]
+
+# Part 1
+
+print("Part 1: ")
+
+# Part 2
+
+print("Part 2: ")
