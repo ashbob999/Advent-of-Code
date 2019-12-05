856e5fe477a5e07012bc242f0729e2a9c3cc8193
ashbob999 <ashbob999@gmail.com>
2019-12-05 12:14:43 +0000

    Added a 2019 Day template

---
+++ 2019/Day_Template.py
@@ -0,0 +1,14 @@
+from util import input_handler
+
+# link to Day X: https://adventofcode.com/2019/day/X
+
+# gets the input lines for the challenge
+lines = input_handler.get_input(X)
+
+# part 1
+
+print("Part 1: ")
+
+# part 2
+
+print("Part 2: ")
