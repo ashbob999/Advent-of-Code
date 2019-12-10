7f8a401780f28bf36f186f2ff38540becd8539e1
ashbob999 <ashbob999@gmail.com>
2019-12-10 13:57:28 +0000

    Fixed day 8 Imports. Added exported image file.

--- 2019/Day 8 - Space Image Format/Code_08.py
+++ 2019/Day 8 - Space Image Format/Code_08.py
@@ -1,14 +1,15 @@
-import numpy as np
 from PIL import Image
 
-with open("Data_08.txt", "r") as file:
-    lines = [line.strip() for line in file]
+from util import input_handler
+
+lines = input_handler.get_input(8)
 
 pixels = list(map(int, list(lines[0])))
 
+
 def chunks(l, n):
     n = max(1, n)
-    return [l[i:i+n] for i in range(0, len(l), n)]
+    return [l[i:i + n] for i in range(0, len(l), n)]
 
 
 w = 25
@@ -18,24 +19,22 @@ print(length)
 
 # part 1
 
-#rows = chunks(pixels, length//6)
-layers = chunks(pixels, w*h)
+# rows = chunks(pixels, length//6)
+layers = chunks(pixels, w * h)
 print(len(layers))
-
 
 layer_num = -1
 fewest_zeros = 100000000
 
 for i, layer in enumerate(layers):
-	zero_count = 0
-	for num in layer:
-		if num == 0:
-			zero_count += 1
-	
-	if zero_count < fewest_zeros:
-		fewest_zeros = zero_count
-		layer_num = i
-		
+    zero_count = 0
+    for num in layer:
+        if num == 0:
+            zero_count += 1
+
+    if zero_count < fewest_zeros:
+        fewest_zeros = zero_count
+        layer_num = i
 
 print(layer_num)
 print(fewest_zeros)
@@ -45,35 +44,28 @@ print("Part 1: ", num_1_mult_2)
 
 # part 2
 
-final_layer = [0] * (w*h)
+final_layer = [0] * (w * h)
 
 for i in range(len(final_layer)):
-	for layer in layers:
-		if layer[i] != 2:
-			final_layer[i] = layer[i]
-			break
+    for layer in layers:
+        if layer[i] != 2:
+            final_layer[i] = layer[i]
+            break
 
 print("Part 2:")
 
 for r in chunks(final_layer, w):
-	print("".join(str(e) for e in r).replace("0", " ").replace("1", "\u2588"))
-
-
-color_1d = [(0, 0, 0) if c == 0 else (255, 255, 255) for c in final_layer]
-
-color_3d = np.reshape(color_1d, (h, w, 3))
-
-#print(color_3d)
+    print("".join(str(e) for e in r).replace("0", " ").replace("1", "\u2588"))
 
 img = Image.new("RGB", (w, h))
 
 pixels = img.load()
 
 for i, v1 in enumerate(chunks(final_layer, w)):
-	for j, v2 in enumerate(v1):
-		if v2 == 0:
-			pixels[j, i] = (0, 0, 0)
-		else:
-			pixels[j, i] = (255, 255, 255)
+    for j, v2 in enumerate(v1):
+        if v2 == 0:
+            pixels[j, i] = (0, 0, 0)
+        else:
+            pixels[j, i] = (255, 255, 255)
 
 img.save("image.png")
