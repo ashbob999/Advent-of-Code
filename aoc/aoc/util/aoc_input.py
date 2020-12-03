5e73a0ca23078555f071c36cc20c8bb963c4ed97
ashbob999 <ashbob999@gmail.com>
2020-12-03 14:42:56 +0000

    Changed default map function to int.

--- aoc/aoc/util/aoc_input.py
+++ aoc/aoc/util/aoc_input.py
@@ -5,8 +5,8 @@ class Aoc_Input:
 	def __init__(self, input_text: str) -> None:
 		self.text: str = input_text
 
-	def to_list(self, mf=str, sep: str = ",") -> list:
+	def to_list(self, mf=int, sep: str = ",") -> list:
 		return [mf(x) for x in self.text.split(sep) if x]
 
-	def to_gen(self, mf=str, sep: str = ",") -> Iterator:
+	def to_gen(self, mf=int, sep: str = ",") -> Iterator:
 		return (mf(x) for x in self.text.split(sep) if x)
