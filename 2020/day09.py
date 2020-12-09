bcfe63b14c63a28182e38d6fe877a3882a29ec4f
ashbob999 <ashbob999@gmail.com>
2020-12-09 12:21:45 +0000

    Optimised day 9.

--- 2020/day09.py
+++ 2020/day09.py
@@ -1,11 +1,18 @@
-from typing import Callable
 from os.path import isfile, join as path_join
+from typing import Callable
+
 file_name = path_join('input', 'day09.txt')
+
+
 def to_list(mf: Callable = int, sep='\n'): return [mf(x) for x in open(file_name).read().split(sep) if x]
+
+
 def to_gen(mf: Callable = int, sep='\n'): return (mf(x) for x in open(file_name).read().split(sep) if x)
+
 
 if not isfile(file_name):
 	from aoc import get_input_file
+
 	get_input_file(session_path=['..', '.env'])
 
 data = to_list()
@@ -41,17 +48,29 @@ def part2(invalid):
 	max_len = 0
 	max_i = 0
 
-	for i in range(len(data)):
-		for j in range(i+1, len(data)):
-			s = sum(data[i:j+1])
-			if s == invalid:
-				if j-i+1 > max_len:
-					max_len = j-i+1
-					max_i = i
-			elif s > invalid:
-				break
+	i = 0
+	j = 0
+	s = data[i]
 
-	print(min(data[max_i:max_i+max_len]) + max(data[max_i:max_i+max_len]))
+	while i < len(data):
+		if s < invalid:
+			j += 1
+			if j < len(data):
+				s += data[j]
+
+		elif s > invalid:
+			s -= data[i]
+			i += 1
+
+		else:
+			if j - i + 1 > max_len:
+				max_len = j - i + 1
+				max_i = i
+
+			s -= data[i]
+			i += 1
+
+	print(min(data[max_i:max_i + max_len]) + max(data[max_i:max_i + max_len]))
 
 
 invalid = part1()
