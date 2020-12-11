00f0922b2d8569cacff2e0c76eb0ad0f4283b7a3
ashbob999 <ashbob999@gmail.com>
2020-12-11 14:06:38 +0000

    Optimised day 11.

--- 2020/day11.py
+++ 2020/day11.py
@@ -1,30 +1,21 @@
-from typing import Callable
 from os.path import isfile, join as path_join
+from typing import Callable
+
 file_name = path_join('input', 'day11.txt')
+
+
 def to_list(mf: Callable = int, sep='\n'): return [mf(x) for x in open(file_name).read().split(sep) if x]
+
+
 def to_gen(mf: Callable = int, sep='\n'): return (mf(x) for x in open(file_name).read().split(sep) if x)
+
 
 if not isfile(file_name):
 	from aoc import get_input_file
+
 	get_input_file(session_path=['..', '.env'])
 
-
-data = to_list(mf=str)
-
-adata = """L.LL.LL.LL
-LLLLLLL.LL
-L.L.L..L..
-LLLL.LL.LL
-L.LL.LL.LL
-L.LLLLL.LL
-..L.L.....
-LLLLLLLLLL
-L.LLLLLL.L
-L.LLLLL.LL""".split("\n")
-
-data = [list(s) for s in data]
-
-#floor = [v[:] for v in data]
+data = to_list(mf=list)
 
 h = len(data)
 w = len(data[0])
@@ -33,20 +24,19 @@ def part1():
 
 	floor = [v[:] for v in data]
 
-
 	while True:
 		changed = 0
 
-		nf = [f[:] for f in floor]
+		nf = [r[:] for r in floor]
 
 		for y in range(h):
 			for x in range(w):
 				adj = 0
-				if y > 0 and floor[y-1][x] == "#":
+				if y > 0 and floor[y - 1][x] == "#":
 					adj += 1
-				if y < h-1 and floor[y+1][x] == "#":
+				if y < h - 1 and floor[y + 1][x] == "#":
 					adj += 1
-				if x > 0 and floor[y][x-1] == "#":
+				if x > 0 and floor[y][x - 1] == "#":
 					adj += 1
 				if x < w-1 and floor[y][x+1] == "#":
 					adj += 1
@@ -59,24 +49,16 @@ def part1():
 				if y < h-1 and x < w-1 and floor[y+1][x+1] == "#":
 					adj += 1
 
-				#print(x,y,adj)
-
 				if floor[y][x] == "L" and adj == 0:
 					nf[y][x] = "#"
 					changed += 1
-					#print(y,x, floor[y][x], nf[y][x])
 
 				elif floor[y][x] == "#" and adj >= 4:
 					nf[y][x] = "L"
 					changed += 1
 
-		floor = [v[:] for v in nf]
+		floor = nf
 
-		for l in floor:
-			pass #print("".join(l))
-
-		print(changed)
-		#break
 		if changed == 0:
 			break
 
@@ -86,13 +68,14 @@ def part1():
 			if floor[y][x] == "#":
 				c += 1
 
-	print("part 1", c)
+	print(c)
 
-def dir(f, w, h, x, y, dy, dx):
+
+def dir(f, x, y, dy, dx):
 	x += dx
 	y += dy
 
-	while x>=0 and y>=0 and x<w and y<h:
+	while x >= 0 and y >= 0 and x < w and y < h:
 		if f[y][x] == "L":
 			return False
 
@@ -104,10 +87,12 @@ def dir(f, w, h, x, y, dy, dx):
 
 	return False
 
+
+dirs = ((-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1))
+
+
 def part2():
-
 	floor = [v[:] for v in data]
-
 
 	while True:
 		changed = 0
@@ -117,41 +102,40 @@ def part2():
 		for y in range(h):
 			for x in range(w):
 				adj = 0
-				if dir(floor, w, h, x, y, -1, 0):
-					adj += 1
-				if dir(floor, w, h, x, y, 1, 0):
-					adj += 1
-				if dir(floor, w, h, x, y, 0, -1):
-					adj += 1
-				if dir(floor, w, h, x, y, 0, 1):
-					adj += 1
-				if dir(floor, w, h, x, y, -1, -1):
-					adj += 1
-				if dir(floor, w, h, x, y, -1, 1):
-					adj += 1
-				if dir(floor, w, h, x, y, 1, -1):
-					adj += 1
-				if dir(floor, w, h, x, y, 1, 1):
-					adj += 1
 
-				#print(x,y,adj)
+				for dir in dirs:
+					tmp_x = x + dir[1]
+					tmp_y = y + dir[0]
+
+					while tmp_x >= 0 and tmp_y >= 0 and tmp_x < w and tmp_y < h:
+						if floor[tmp_y][tmp_x] == "L":
+							break
+
+						if floor[tmp_y][tmp_x] == "#":
+							adj += 1
+							break
+
+						tmp_x += dir[1]
+						tmp_y += dir[0]
+
+				# print(x,y,adj)
 
 				if floor[y][x] == "L" and adj == 0:
 					nf[y][x] = "#"
 					changed += 1
-					#print(y,x, floor[y][x], nf[y][x])
+				# print(y,x, floor[y][x], nf[y][x])
 
 				elif floor[y][x] == "#" and adj >= 5:
 					nf[y][x] = "L"
 					changed += 1
 
-		floor = [v[:] for v in nf]
+		floor = nf  # [v[:] for v in nf]
 
-		for l in floor:
-			pass #print("".join(l))
+		# for l in floor:
+		# 	print("".join(l))
 
-		print(changed)
-		#break
+		# print(changed)
+		# break
 		if changed == 0:
 			break
 
@@ -161,7 +145,7 @@ def part2():
 			if floor[y][x] == "#":
 				c += 1
 
-	print("part 2", c)
+	print(c)
 
 
 part1()
