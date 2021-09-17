c2ef32f6ffa3728d74d6fa0c63ea3864add41517
ashbob999 <ashbob999@gmail.com>
2021-09-17 17:03:57 +0100

    finished 2015 cpp solutions

--- cpp/2015/main.cpp
+++ cpp/2015/main.cpp
@@ -60,16 +60,19 @@ int main()
 	auto cd = d24;
 
 	vector<BaseDay*> days = {
-			cd
+		d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15, d16, d17, d18, d19, d20, d21, d22, d23, d24, d25
+		//cd
 	};
 
-	runDays(days, { 10 });
+	runDays(days, { 2 });
 
+	/*
 	cd->load_input();
 	auto res = cd->solve();
 	cd->unload_input();
 	cout << res.first << " : " << res.second << endl;
 	// cout << cd->stringResult.first << " : " << cd->stringResult.second << endl;
+	*/
 
 	for (auto& d : days)
 	{
