34c989b0adfd6b3915100984aa2de2f14c6b1d13
ashbob999 <ashbob999@gmail.com>
2021-12-08 13:41:15 +0000

    optimised 2021 day 8

--- 2021/day08.py
+++ 2021/day08.py
@@ -30,6 +30,8 @@ seg_pos = {
 	frozenset([0, 1, 2, 3, 5, 6]):9
 }
 
+char_ord = {c:ord(c) for c in "abcdefg"}
+
 def part1():
 	c=0
 	for v in data:
@@ -43,39 +45,78 @@ def part1():
 
 from itertools import permutations as perms
 
+
+def check_segments(segment_data, segment_mapping) -> bool:
+	for seg in segment_data[0] + segment_data[1]:
+		seg_set = set(map(int, seg.translate(segment_mapping)))
+
+		if frozenset(seg_set) not in seg_pos:
+			return False
+
+	return True
+
+
+def calc_number(segment_data: list, mapping: dict) -> int:
+	num = 0
+	for seg in segment_data[1]:
+		seg_set = frozenset(map(int, seg.translate(mapping)))
+		n = seg_pos[seg_set]
+		num *= 10
+		num += n
+
+	return num
+
+
+def interleave(original, new, indices: dict) -> str:
+	result = original
+	for k, v in indices.items():
+		result = result[:v] + new[k] + result[v:]
+
+	return result
+
+
+def get_sum(segment_data, left_values, poss_values, indices) -> int:
+	poss_len = len(poss_values)
+	left_len = len(left_values)
+	for poss_seg in perms(poss_values, poss_len):
+		for left_seg in perms(left_values, left_len):
+			p = interleave("".join(left_seg), "".join(poss_seg), indices)
+
+			mapping = {char_ord[c]: str(i) for i, c in enumerate(p)}
+
+			success = check_segments(segment_data, mapping)
+			if success:
+				return calc_number(segment_data, mapping)
+
+	return 0
+
+
 def part2():
 	s = 0
-	#={i:list("abcdefg") for i in range(7)}
 	for v in data:
-		for p in perms("abcdefg", 7):
-			mapping = {ord(c):str(i) for i, c in enumerate(p)}
-			
-			success=True
-			
-			cv = 0
-			mp={}
-			
-			for seg in v[0] + v[1]:
-				seg_set = set(map(int, seg.translate(mapping)))
-				
-				if frozenset(seg_set) in seg_pos:
-					cv += 1
-					mp[frozenset(seg)] = seg_pos[frozenset(seg_set)]
-				else:
-					success = False
-					break
-			
-			if success:
-				num = 0
-				for seg in v[1]:
-					seg_set = set(map(int, seg.translate(mapping)))
-					n = seg_pos[frozenset(seg_set)]
-					num *= 10
-					num += n
-					
-				s += num
-				
-				break
+		# count of 2/3/4 unique (also 7 but ignoring) -> numbers 1/4/7 (ignoring number 8)
+		count = {2:0, 3:0, 4:0}
+		poss_segs = set()
+		for seg in v[0] + v[1]:
+			if len(seg) <= 4:
+				count[len(seg)] += 1
+				poss_segs |= set(seg)
+
+		left_values = set("abcdefg") ^ poss_segs
+		left_values = sorted(list(left_values))
+		poss_values = sorted(list(poss_segs))
+
+		if len(poss_segs) == 2:  # just 1
+			indices = {0:2, 1:5}
+		elif len(poss_segs) == 3:  # just 1/7
+			indices = {0:0, 1:2, 2:5}
+		elif len(poss_segs) == 4:  # just 1/4
+			indices = {0:1, 1:2, 2:3, 3:5}
+		else:  # just 1/4/7
+			indices = {0:0, 1:1, 2:2, 3:3, 4:5}
+
+		cs = get_sum(v, left_values, poss_values, indices)
+		s += cs
 
 	return s
 
