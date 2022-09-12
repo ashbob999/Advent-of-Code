8c1e2dcb68b2a50c5291e050314fc6123266aeaa
ashbob999 <ashbob999@gmail.com>
2022-09-12 13:47:35 +0100

    added cpp 2021 base files

---
+++ cpp/2021/day01.cpp
@@ -0,0 +1,14 @@
+#include "../aocHelper.h"
+
+class Day01 : public BaseDay
+{
+public:
+	Day01() : BaseDay("01") {}
+
+	result_type solve() override
+	{
+		long long part1 = 0, part2 = 0;
+
+		return { part1, part2 };
+	}
+};
