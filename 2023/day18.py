fbd609fdd914cae5b46df4db48d15d5d50295f5e
ashbob999 <ashbob999@gmail.com>
2023-12-18 18:37:52 +0000

    Optimised 2023 day 18

--- 2023/day18.py
+++ 2023/day18.py
@@ -24,86 +24,6 @@ dirs = {
 	"D": (0, 1)
 }
 
-def fill(grid, top_left, width, height, start):
-	if start in grid:
-		return set()
-	
-	to_check = []
-	found = set()
-	to_check.append(start)
-	
-	while len(to_check) > 0:
-		curr = to_check.pop(0)
-		found.add(curr)
-		
-		for dir in dirs.values():
-			nx = curr[0] + dir[0]
-			ny = curr[1] + dir[1]
-			
-			if top_left[0] <= nx < top_left[0] + width:
-				if top_left[1] <= ny < top_left[1] + height:
-					n = (nx, ny)
-					if n not in grid and n not in found and n not in to_check:
-						to_check.append(n)
-					
-	return found
-
-
-def part1():
-	grid = set()
-	path = []
-	start = (0, 0)
-	curr = start
-	grid.add(curr)
-	path.append(curr)
-	
-	for d in data:
-		dir = dirs[d[0]]
-		amnt = d[1]
-		
-		for i in range(amnt):
-			curr = (curr[0]+dir[0], curr[1]+dir[1])
-			grid.add(curr)
-			path.append(curr)
-	
-	minx = min(map(lambda v:v[0], grid))
-	maxx = max(map(lambda v:v[0], grid))
-	
-	miny = min(map(lambda v:v[1], grid))
-	maxy = max(map(lambda v:v[1], grid))
-	
-	width = maxx - minx +1
-	height = maxy - miny +1
-	top_left = (minx, miny)
-	
-	outside = set()
-	
-	# top
-	for x in range(width):
-		p = (top_left[0] + x, top_left[1])
-		if p not in outside:
-			outside |= fill(grid, top_left, width, height, p)
-		
-	# bottom
-	for x in range(width):
-		p = (top_left[0] + x, top_left[1] + height - 1)
-		if p not in outside:
-			outside |= fill(grid, top_left, width, height, p)
-	
-	# left
-	for y in range(height):
-		p = (top_left[0], top_left[1] + y)
-		if p not in outside:
-			outside |= fill(grid, top_left, width, height, p)
-			
-	# right
-	for y in range(height):
-		p = (top_left[0] + width - 1, top_left[1] + y)
-		if p not in outside:
-			outside |= fill(grid, top_left, width, height, p)
-
-	return width*height - len(outside)
-
 
 # shoelace formula
 def polygonArea(polygon):
@@ -118,6 +38,30 @@ def polygonArea(polygon):
 	
 	# Return absolute value
 	return int(abs(area / 2.0))
+	
+
+def part1():
+	path = []
+	start = (0, 0)
+	curr = start
+	path.append(curr)
+	
+	boundary = 0
+	
+	for d in data:
+		dir = dirs[d[0]]
+		amnt = d[1]
+			
+		boundary += amnt
+		
+		curr = (curr[0] + amnt * dir[0], curr[1] + amnt * dir[1])
+		path.append(curr)
+	
+	area = polygonArea(path)
+	
+	# picks theoreom
+	res = area + (boundary//2) + 1
+	return res
 
 
 def part2():
