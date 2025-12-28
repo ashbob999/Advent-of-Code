4358f9d89d04d635a81481ab23abfca68989f969
ashbob999 <ashbob999@gmail.com>
2025-12-28 11:41:24 +0000

    Removed days 13-25 from the cpp main.cpp template

--- cpp/template_main.cpp
+++ cpp/template_main.cpp
@@ -10,8 +10,7 @@ int main()
 
 	auto&& cd = d1;
 
-	vector<BaseDay*> days = {// &d1, &d2, &d3, &d4, &d5, &d6, &d7, &d8, &d9, &d10, &d11, &d12, &d13, &d14, &d15, &d16,
-							 // &d17, &d18, &d19, &d20, &d21, &d22, &d23, &d24, &d25
+	vector<BaseDay*> days = {// &d1, &d2, &d3, &d4, &d5, &d6, &d7, &d8, &d9, &d10, &d11, &d12,
 							 &cd};
 
 	// runDays(days, {1000});
